175546d0ab9f8baa352933d4c0d3f72e93e6ad9a
surendrasinghshekhawat <surendrasingh55556666@gmail.com>
2025-06-09 14:49:33 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,151 @@
+const statsData = {
+  "2025": {
+    orangeCap: [
+      { player: "Virat Kohli", runs: 732 },
+      { player: "Shubman Gill", runs: 695 },
+      { player: "Ruturaj Gaikwad", runs: 670 },
+      { player: "Jos Buttler", runs: 660 },
+      { player: "David Warner", runs: 645 },
+      { player: "Faf du Plessis", runs: 635 },
+      { player: "KL Rahul", runs: 620 },
+      { player: "Suryakumar Yadav", runs: 610 },
+      { player: "Rinku Singh", runs: 590 },
+      { player: "Sanju Samson", runs: 580 },
+    ],
+    mostFours: [
+      { player: "Shubman Gill", fours: 88 },
+      { player: "Virat Kohli", fours: 85 },
+      { player: "Ruturaj Gaikwad", fours: 80 },
+      { player: "KL Rahul", fours: 76 },
+      { player: "David Warner", fours: 74 },
+      { player: "Faf du Plessis", fours: 72 },
+      { player: "Yashasvi Jaiswal", fours: 70 },
+      { player: "Sanju Samson", fours: 68 },
+      { player: "Rinku Singh", fours: 65 },
+      { player: "Ishan Kishan", fours: 62 },
+    ],
+    mostSixes: [
+      { player: "Rinku Singh", sixes: 45 },
+      { player: "Jos Buttler", sixes: 44 },
+      { player: "Faf du Plessis", sixes: 42 },
+      { player: "Suryakumar Yadav", sixes: 41 },
+      { player: "MS Dhoni", sixes: 39 },
+      { player: "Glenn Maxwell", sixes: 38 },
+      { player: "Andre Russell", sixes: 37 },
+      { player: "Hardik Pandya", sixes: 36 },
+      { player: "Rahul Tewatia", sixes: 35 },
+      { player: "Ruturaj Gaikwad", sixes: 34 },
+    ],
+    mostCenturies: [
+      { player: "Shubman Gill", centuries: 3 },
+      { player: "Virat Kohli", centuries: 2 },
+      { player: "Jos Buttler", centuries: 2 },
+      { player: "Yashasvi Jaiswal", centuries: 1 },
+      { player: "KL Rahul", centuries: 1 },
+      { player: "Ruturaj Gaikwad", centuries: 1 },
+      { player: "Suryakumar Yadav", centuries: 1 },
+      { player: "Faf du Plessis", centuries: 1 },
+      { player: "David Warner", centuries: 1 },
+      { player: "Rinku Singh", centuries: 1 },
+    ],
+    mostFifties: [
+      { player: "Virat Kohli", fifties: 6 },
+      { player: "Ruturaj Gaikwad", fifties: 6 },
+      { player: "Shubman Gill", fifties: 5 },
+      { player: "KL Rahul", fifties: 5 },
+      { player: "David Warner", fifties: 5 },
+      { player: "Sanju Samson", fifties: 4 },
+      { player: "Faf du Plessis", fifties: 4 },
+      { player: "Yashasvi Jaiswal", fifties: 4 },
+      { player: "Rinku Singh", fifties: 4 },
+      { player: "Ishan Kishan", fifties: 4 },
+    ]
+  },
+  "2024": {
+    orangeCap: [
+      { player: "Shubman Gill", runs: 730 },
+      { player: "Virat Kohli", runs: 710 },
+      { player: "KL Rahul", runs: 695 },
+      { player: "Faf du Plessis", runs: 680 },
+      { player: "Ruturaj Gaikwad", runs: 670 },
+      { player: "David Warner", runs: 655 },
+      { player: "Suryakumar Yadav", runs: 640 },
+      { player: "Sanju Samson", runs: 625 },
+      { player: "Rinku Singh", runs: 610 },
+      { player: "Jos Buttler", runs: 600 },
+    ],
+    mostFours: [
+      { player: "Shubman Gill", fours: 87 },
+      { player: "KL Rahul", fours: 84 },
+      { player: "Ruturaj Gaikwad", fours: 82 },
+      { player: "Virat Kohli", fours: 80 },
+      { player: "David Warner", fours: 78 },
+      { player: "Faf du Plessis", fours: 75 },
+      { player: "Sanju Samson", fours: 72 },
+      { player: "Ishan Kishan", fours: 70 },
+      { player: "Rinku Singh", fours: 68 },
+      { player: "Suryakumar Yadav", fours: 66 },
+    ],
+    mostSixes: [
+      { player: "Jos Buttler", sixes: 44 },
+      { player: "Rinku Singh", sixes: 42 },
+      { player: "Faf du Plessis", sixes: 40 },
+      { player: "Suryakumar Yadav", sixes: 39 },
+      { player: "Andre Russell", sixes: 37 },
+      { player: "Glenn Maxwell", sixes: 36 },
+      { player: "KL Rahul", sixes: 35 },
+      { player: "Ruturaj Gaikwad", sixes: 33 },
+      { player: "Yashasvi Jaiswal", sixes: 32 },
+      { player: "Hardik Pandya", sixes: 30 },
+    ],
+    mostCenturies: [
+      { player: "Shubman Gill", centuries: 2 },
+      { player: "Virat Kohli", centuries: 2 },
+      { player: "KL Rahul", centuries: 2 },
+      { player: "Ruturaj Gaikwad", centuries: 1 },
+      { player: "Suryakumar Yadav", centuries: 1 },
+      { player: "Jos Buttler", centuries: 1 },
+      { player: "David Warner", centuries: 1 },
+      { player: "Yashasvi Jaiswal", centuries: 1 },
+      { player: "Faf du Plessis", centuries: 1 },
+      { player: "Rinku Singh", centuries: 1 },
+    ],
+    mostFifties: [
+      { player: "Virat Kohli", fifties: 6 },
+      { player: "Shubman Gill", fifties: 5 },
+      { player: "KL Rahul", fifties: 5 },
+      { player: "Ruturaj Gaikwad", fifties: 5 },
+      { player: "David Warner", fifties: 5 },
+      { player: "Faf du Plessis", fifties: 4 },
+      { player: "Sanju Samson", fifties: 4 },
+      { player: "Yashasvi Jaiswal", fifties: 4 },
+      { player: "Suryakumar Yadav", fifties: 4 },
+      { player: "Rinku Singh", fifties: 4 },
+    ]
+  },
+};
+
+// let button=document.querySelectorAll(".button");
+// button.addEventListner("clcik",()=>{
+for (let a in statsData) {
+  if (typeof statsData[a] === "object") {
+    for (let b in statsData[a]) {
+        document.writeln(b + " "+ statsData[a][b] + "<br>")
+        // let statsData1=Object.entries(statsData);
+        // function fName(a,b){
+        //   statsData1.forEach((b)=>{
+        //     document.writeln(b)
+        //   })
+        // } 
+        // fName(a,b);
+    }
+  }
+  else{
+  document.writeln(a + " " + statsData[a] + "<br>");
+  }
+}
+
+// })
+
+
+
